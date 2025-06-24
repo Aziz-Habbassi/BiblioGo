66b60e17456be63cf5b99abeeb3f2d559f9d5119
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-06-24 19:35:09 +0100

    add fonts and best seller list

--- lib/features/views/home_view/home_view.dart
+++ lib/features/views/home_view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bibliogo/core/utils/assets.dart';
 import 'package:bibliogo/features/views/home_view/widgets/covers_list_view.dart';
 import 'package:bibliogo/features/views/home_view/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,46 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: CustomAppBar(), body: CoversListView());
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: CoversListView()),
+          SliverToBoxAdapter(
+            child: Text(
+              "Best Seller",
+              style: TextStyle(
+                fontFamily: "SectraFine",
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          SliverList.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Row(
+                children: [
+                  Image.asset(cover, height: 200),
+                  Column(
+                    children: [
+                      Text(
+                        "Harry Potter and the goblet of Fire",
+                        style: TextStyle(
+                          fontFamily: "SectraFine",
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text("J.k Rowing"),
+                    ],
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/features/views/splash_view/splash_view.dart
+++ lib/features/views/splash_view/splash_view.dart
@@ -16,6 +16,7 @@ class _SplashViewState extends State<SplashView>
   void initState() {
     super.initState();
     Future.delayed(Duration(seconds: 3), () {
+      if (!mounted) return;
       Navigator.pushReplacementNamed(context, HomeView.id);
     });
   }
