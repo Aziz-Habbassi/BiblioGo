bd813cd8a27eb5e211b79fdfb23a206c71b6c4c8
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-07-04 14:13:09 +0100

    search books cubit

--- lib/features/search/presentations/manager/search_books_cubit/search_books_cubit.dart
+++ lib/features/search/presentations/manager/search_books_cubit/search_books_cubit.dart
@@ -1,8 +1,25 @@
+import 'package:bibliogo/core/errors/failures.dart';
+import 'package:bibliogo/core/models/book_model/book_model.dart';
+import 'package:bibliogo/features/search/data/repos/search_repo_impl.dart';
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
-
 part 'search_books_state.dart';
 
 class SearchBooksCubit extends Cubit<SearchBooksState> {
-  SearchBooksCubit() : super(SearchBooksInitial());
+  SearchBooksCubit(this.searchRepoImpl) : super(SearchBooksInitial());
+  final SearchRepoImpl searchRepoImpl;
+  Future<void> searchBooks({required String subject}) async {
+    emit(SearchBooksLoading());
+    final Either<Failures, List<BookModel>> result = await searchRepoImpl
+        .searchBooks(subject: subject);
+    result.fold(
+      (failure) {
+        emit(SearchBooksFailed(errMessage: failure.errMessage));
+      },
+      (books) {
+        emit(SearchBooksSuccess(books: books));
+      },
+    );
+  }
 }
--- lib/features/search/presentations/manager/search_books_cubit/search_books_state.dart
+++ lib/features/search/presentations/manager/search_books_cubit/search_books_state.dart
@@ -5,8 +5,16 @@ sealed class SearchBooksState {}
 
 final class SearchBooksInitial extends SearchBooksState {}
 
-final class SearchBooksSuccess extends SearchBooksState {}
+final class SearchBooksSuccess extends SearchBooksState {
+  final List<BookModel> books;
+
+  SearchBooksSuccess({required this.books});
+}
 
 final class SearchBooksLoading extends SearchBooksState {}
 
-final class SearchBooksFailed extends SearchBooksState {}
+final class SearchBooksFailed extends SearchBooksState {
+  final String errMessage;
+
+  SearchBooksFailed({required this.errMessage});
+}
